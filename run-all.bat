51aa73a28ca55fd82e1105a76d63aa1e50746f5a
Larry Costigan 5 <60368346+larrycostigan5FDC@users.noreply.github.com>
2024-09-11 09:21:09 -0400

    Allow commets in list file

--- run-all.bat
+++ run-all.bat
@@ -7,7 +7,13 @@ for /r %%i in (cfg\*) do start /D "%~dp0" python run.py %%~ni
 GOTO end
 
 :lst
-for /F "tokens=*" %%A in (%1.lst) do start /D "%~dp0" python run.py %%A
+for /F "tokens=*" %%A in (%1.lst) do (
+    if /i "%%A:~0,1%"=="#" (
+        echo "skip %%A"
+    ) else (
+        start /D "%~dp0" python run.py %%A
+    )
+)
 
 :end
 exit
